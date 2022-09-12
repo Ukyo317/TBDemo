76af99cc5af6084306047f7d6a50b5642a40b20c
Ukyo317 <chenyuqichn@hotmail.com>
2022-09-12 16:24:27 +0800

    add new branch

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,7 +10,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [],
-  pathPrefix: "/blog",
+  pathPrefix: "/TBDemo",
 };
 
 export default config;
